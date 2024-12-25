e054ac72102834073ae5981bb8d9071582b81b68
Nanda-dvps <pnr956244@gmail.com>
2024-12-25 18:28:35 +0000

    jenkins

---
+++ data.tf
@@ -0,0 +1,21 @@
+
+data "aws_ami" "ami_info" {
+
+    most_recent = true
+    owners = ["034362035671"]
+
+    filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "zone_name" {
+  type        = string
+  default     = "qkeats4u.online "
+  description = "description"
+}
